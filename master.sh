8bd7862bd32b229b864cbb27b666ecb05fc0fc37
Robby <77552033+TinyRobby@users.noreply.github.com>
2021-06-02 12:34:18 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,11 +2,12 @@
 cd ~
 git clone https://github.com/Botspot/pi-apps 
 cd pi-apps
-echo [Desktop Entry] | tee $HOME/Desktop/pi-apps.desktop
+echo [Desktop Entry] 
+touch $HOME/Desktop/pi-apps.desktop
 echo Name=Pi Apps | tee $HOME/Desktop/pi-apps.desktop
 echo Exec=$HOME/pi-apps/gui | tee $HOME/Desktop/pi-apps.desktop
 echo Icon=$HOME/pi-apps/icons/logo-64.png | tee $HOME/Desktop/pi-apps.desktop
 echo Type=Application | tee $HOME/Desktop/pi-apps.desktop
 echo Categories=Internet  | tee $HOME/Desktop/pi-apps.desktop
-echo :)
+echo :\)
 exit
